720868e4d30edee040fb1f149185bf423ccd32e4
Jonas Bandi <dev (at) jonasbandi (dot) net>
2015-06-18 22:39:18 +0200

    priority and updating sent tweets

--- bin/birdcage.js
+++ bin/birdcage.js
@@ -3,26 +3,36 @@ module.exports = function (configuration) {
 
     let request = require('request'),
         Twit = require('twit'),
-        util = require('util');
+        util = require('util'),
+        https = require("https");
+
+    let perform_loop = true;
+
+    const FIREBASE_BASE = 'https://scorching-inferno-3523.firebaseio.com';
+    const REST_AUTH = `auth=${configuration.firebase_secret}`;
+    const URL_GET_USERS = `${FIREBASE_BASE}/users.json?${REST_AUTH}`;
 
     return {
         post: post
     };
 
     function post() {
-        request('https://scorching-inferno-3523.firebaseio.com/users.json?auth=v0rJhuNr79II1naM09CyJwVkI5qO62ll3ANnkrHN', function (error, response, body) {
+        request(URL_GET_USERS, function (error, response, body) {
+            console.log("Get users response: " + response.statusCode);
             let users = JSON.parse(body);
-            for (let user in users) {
-                console.log("Checking tweets for user: " + user);
-                tweetForUser(users[user]);
+            for (let userId in users) {
+                console.log("Checking tweets for user: " + userId);
+                var user = users[userId];
+                user.id = userId;
+                tweetForUser(user);
             }
         })
     }
 
     function tweetForUser(user) {
         console.log(util.inspect(user));
-        request('https://scorching-inferno-3523.firebaseio.com/posts/' + user.id + '/.json?auth=v0rJhuNr79II1naM09CyJwVkI5qO62ll3ANnkrHN', function (error, response, body) {
-            console.log("Response: " + response.statusCode);
+        request(`${FIREBASE_BASE}/posts/` + user.id + '/.json?${REST_AUTH}&orderBy="$priority"&startAt=1&limitToFirst=1', function (error, response, body) {
+            console.log("Get latest tweet response: " + response.statusCode);
             if (!error && response.statusCode == 200) {
                 console.log("Got tweets: " + body);
                 let tweets = JSON.parse(body);
@@ -39,11 +49,25 @@ module.exports = function (configuration) {
                     });
 
                     T.post('statuses/update', {status: tweet.content + new Date()}, function (err, data, response) {
-                        console.log(util.inspect(response.statusCode));
-                    })
+                        console.log('Twitter post response: ' + util.inspect(response.statusCode));
+                    });
+
+                    var req = https.request({
+                            hostname: 'scorching-inferno-3523.firebaseio.com',
+                            method: "PATCH",
+                            path: '/posts/test_jb/' + tweetId + `/.json?${REST_AUTH}`
+                        }, function (res) {
+                            console.log("Firebase update response: " + util.inspect(res.statusCode));
+                        }
+
+                    );
+                    req.end(JSON.stringify({
+                        ".priority": perform_loop ? Date.now() : 1,
+                        last_sent: new Date(),
+                        sent_count: tweet.sent_count !== undefined ? ++tweet.sent_count : 0
+                    }));
                 }
             }
         });
     }
-
 };
