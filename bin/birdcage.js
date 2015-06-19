eff622be241d044283ddcc6b1150857a6f34bb88
Jonas Bandi <reg@jonasbandi.net>
2015-06-19 20:38:40 +0000

    From Cloud9

--- bin/birdcage.js
+++ bin/birdcage.js
@@ -18,7 +18,7 @@ module.exports = function (configuration) {
 
     function post() {
         request(URL_GET_USERS, function (error, response, body) {
-            console.log("Get users response: " + response.statusCode);
+            console.log("Get users response : " + response.statusCode);
             let users = JSON.parse(body);
             for (let userId in users) {
                 console.log("Checking tweets for user: " + userId);
